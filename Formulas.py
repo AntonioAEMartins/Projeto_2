244552011b7a1a1f8309f838bea7916cc46a15cf
Antônio A E Martins <81169982+AntonioAEMartins@users.noreply.github.com>
2021-05-02 17:39:48 -0300

    Informações

--- Formulas.py
+++ Formulas.py
@@ -4,4 +4,12 @@
 #Formulas Condução
 d/k*A
 
+#Informações da Chaleira
+#Chaleira como Cilindro
+Raio = 10.5 #Cm
+Altura =
+Condutividade_t=
+Largura_parede_i=
+Pot= 1200 #Watts
+V_max= 1.7 #Litros
 
